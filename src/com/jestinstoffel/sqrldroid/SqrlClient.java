edfe58d2d8bc248eb61c670ac8e31111c1d6b625
Jestin Stoffel <jestin.stoffel@gmail.com>
2013-12-15 15:01:31 -0600

    finished implementing the sqrl client class, but will have to decided which exceptions to handle locally and which to allow to bubble.

--- src/com/jestinstoffel/sqrldroid/SqrlClient.java
+++ src/com/jestinstoffel/sqrldroid/SqrlClient.java
@@ -1,6 +1,9 @@
 package com.jestinstoffel.sqrldroid;
 
+import java.security.MessageDigest;
+import java.security.SecureRandom;
 import java.util.Arrays;
+import java.util.Locale;
 
 import com.jestinstoffel.sqrldroid.crypto.HmacGenerator;
 import com.jestinstoffel.sqrldroid.crypto.PbkdfHandler;
@@ -11,6 +14,7 @@ public class SqrlClient implements SqrlClientHandler {
 	private HmacGenerator mHmac;
 	private PbkdfHandler mPbkdf;
 	private SqrlSigner mSigner;
+	private SecureRandom mPrng;
 	
 	public SqrlClient(
 			HmacGenerator hmac,
@@ -19,6 +23,7 @@ public class SqrlClient implements SqrlClientHandler {
 		mHmac = hmac;
 		mPbkdf = pbkdf;
 		mSigner = signer;
+		mPrng = new SecureRandom();
 	}
 
 	@Override
@@ -64,48 +69,118 @@ public class SqrlClient implements SqrlClientHandler {
 	}
 
 	@Override
-	public SqrlData GetSqrlDataForLogin(byte[] masterKey, String url) {
-		// TODO Auto-generated method stub
-		return null;
+	public SqrlData GetSqrlDataForLogin(byte[] masterKey, String url) throws Exception {
+		String domain = GetDomainFromUrl(url);
+		byte[] privateKey = mHmac.GeneratePrivateKey(masterKey, domain);
+
+		SqrlData sqrlData = new SqrlData();
+		sqrlData.Url = GetUrlWithoutProtocol(url);
+		sqrlData.Signature = mSigner.Sign(privateKey, GetUrlWithoutProtocol(url));
+		sqrlData.PublicKey = mSigner.MakePublicKey(privateKey);
+
+		Arrays.fill(privateKey, (byte)0);
+
+		return sqrlData;
 	}
 
 	@Override
-	public SqrlData GetSqrlDataForLogin(byte[] masterIdentityKey,
-			String password, byte[] salt, String url) {
-		// TODO Auto-generated method stub
-		return null;
+	public SqrlData GetSqrlDataForLogin(byte[] masterIdentityKey, String password, byte[] salt, String url) throws Exception {
+		byte[] masterKey = CalculateMasterKey(masterIdentityKey, password, salt);
+		SqrlData sqrlData = GetSqrlDataForLogin(masterKey, url);
+
+		Arrays.fill(masterKey, (byte)0);
+
+		return sqrlData;
 	}
 
 	@Override
-	public SqrlData GetSqrlDataForLogin(SqrlIdentity identity, String password,
-			String url) {
-		// TODO Auto-generated method stub
-		return null;
+	public SqrlData GetSqrlDataForLogin(SqrlIdentity identity, String password, String url) throws Exception {
+		return GetSqrlDataForLogin(identity.MasterIdentityKey, password, identity.Salt, url);
 	}
 
 	@Override
-	public SqrlIdentity CreateIdentity(String password, byte[] entropy) {
-		// TODO Auto-generated method stub
-		return null;
+	public SqrlIdentity CreateIdentity(String password, byte[] entropy) throws Exception {
+		SqrlIdentity identity = new SqrlIdentity();
+
+		identity.Salt = new byte[8];
+		byte[] masterKey = new byte[32];
+
+		MessageDigest md = MessageDigest.getInstance("SHA-256");
+
+		mPrng.nextBytes(identity.Salt);
+		mPrng.nextBytes(masterKey);
+
+		// XOR the generated master key with the entropy (making any potential backdoors in the implementation of SecureRandom irrelevent)
+		masterKey = Xor(masterKey, md.digest(entropy));
+
+		// call the SCrypt PBKDF to create the password key
+		byte[] passwordKey = mPbkdf.GeneratePasswordKey(password, identity.Salt);
+
+		// get the partial hash for password verification
+		identity.PartialPasswordHash = mPbkdf.GetPartialHashFromPasswordKey(passwordKey);
+
+		// XOR the master key and the password key to get the master identity key
+		identity.MasterIdentityKey = Xor(passwordKey, masterKey);
+
+		Arrays.fill(masterKey, (byte)0);
+		Arrays.fill(passwordKey, (byte)0);
+
+		return identity;
 	}
 
 	@Override
-	public SqrlIdentity ChangePassword(String oldPassword, byte[] oldSalt,
-			String newPassword, byte[] masterIdentityKey) {
-		// TODO Auto-generated method stub
-		return null;
+	public SqrlIdentity ChangePassword(String oldPassword, byte[] oldSalt, String newPassword, byte[] masterIdentityKey) throws Exception {
+		SqrlIdentity identity = new SqrlIdentity();
+
+		// calculate the master key
+		byte[] oldPasswordKey = mPbkdf.GeneratePasswordKey(oldPassword, oldSalt);
+		byte[] masterKey = Xor(oldPasswordKey, masterIdentityKey);
+
+		// generate new salt
+		identity.Salt = new byte[8];
+		mPrng.nextBytes(identity.Salt);
+
+		// generate the new password key
+		byte[] newPasswordKey = mPbkdf.GeneratePasswordKey(newPassword, identity.Salt);
+
+		// get the partial hash for password verification
+		identity.PartialPasswordHash = mPbkdf.GetPartialHashFromPasswordKey(newPasswordKey);
+
+		// XOR the master key and the new password key to get the master identity key
+		identity.MasterIdentityKey = Xor(newPasswordKey, masterKey);
+
+		Arrays.fill(masterKey, (byte)0);
+		Arrays.fill(oldPasswordKey, (byte)0);
+		Arrays.fill(newPasswordKey, (byte)0);
+
+		return identity;
 	}
 
 	@Override
 	public boolean VerifyPassword(String password, SqrlIdentity identity) {
-		// TODO Auto-generated method stub
-		return false;
+		return mPbkdf.VerifyPassword(password, identity.Salt, identity.PartialPasswordHash);
 	}
 
 	@Override
-	public String GetDomainFromUrl(String url) {
-		// TODO Auto-generated method stub
-		return null;
+	public String GetDomainFromUrl(String url) throws Exception {
+		// strip off scheme
+		String domain = GetUrlWithoutProtocol(url);
+
+		int pipeIndex = domain.indexOf('|');
+
+		if(pipeIndex >= 0)
+		{
+			return domain.substring(0, pipeIndex);
+		}
+
+		int slashIndex = domain.indexOf('/');
+
+		if(slashIndex < 0)
+		{
+			throw new Exception("SQRL urls must contain a '/'");
+		}
+
+		return domain.substring(0, slashIndex);
 	}
 
 	private byte[] Xor(byte[] a, byte[] b) throws Exception
@@ -123,5 +198,22 @@ public class SqrlClient implements SqrlClientHandler {
 		}
 
 		return result;
+	}
+	
+	private String GetUrlWithoutProtocol(String url) throws Exception {
+		// only use this variable for validity checking, never for any cryptographic features because ToLower() will modify nonces
+		String lowerUrl = url.toLowerCase(Locale.ENGLISH);
+
+		if(lowerUrl.startsWith("sqrl://"))
+		{
+			return url.substring(7);
+		}
+
+		if(lowerUrl.startsWith("qrl://"))
+		{
+			return url.substring(6);
+		}
+
+		throw new Exception("SQRL urls must begin with 'sqrl://' or 'qrl://'");
 	}
 }
--- src/com/jestinstoffel/sqrldroid/SqrlClientHandler.java
+++ src/com/jestinstoffel/sqrldroid/SqrlClientHandler.java
@@ -3,11 +3,11 @@ package com.jestinstoffel.sqrldroid;
 public interface SqrlClientHandler {
 	byte[] CalculateMasterKey(byte[] masterIdentityKey, String password, byte[] salt) throws Exception;
 	byte[] CalculateMasterIdentityKey(byte[] masterKey, String password, byte[] salt) throws Exception;
-	SqrlData GetSqrlDataForLogin(byte[] masterKey, String url);
-	SqrlData GetSqrlDataForLogin(byte[] masterIdentityKey, String password, byte[] salt, String url);
-	SqrlData GetSqrlDataForLogin(SqrlIdentity identity, String password, String url);
-	SqrlIdentity CreateIdentity(String password, byte[] entropy);
-	SqrlIdentity ChangePassword(String oldPassword, byte[] oldSalt, String newPassword, byte[] masterIdentityKey);
+	SqrlData GetSqrlDataForLogin(byte[] masterKey, String url) throws Exception;
+	SqrlData GetSqrlDataForLogin(byte[] masterIdentityKey, String password, byte[] salt, String url) throws Exception;
+	SqrlData GetSqrlDataForLogin(SqrlIdentity identity, String password, String url) throws Exception;
+	SqrlIdentity CreateIdentity(String password, byte[] entropy) throws Exception;
+	SqrlIdentity ChangePassword(String oldPassword, byte[] oldSalt, String newPassword, byte[] masterIdentityKey) throws Exception;
 	boolean VerifyPassword(String password, SqrlIdentity identity);
-	String GetDomainFromUrl(String url);
+	String GetDomainFromUrl(String url) throws Exception;
 }
